8dcf918a8cf65cc9c267686a0174593f9584a8ae
Minhazul Islam Rikat <minhazulrikat@gmail.com>
2026-09-02 01:17:23 +0600

    tab filtering added

--- script/script.js
+++ script/script.js
@@ -1,34 +1,71 @@
-const getElementById = (id) =>{
-    return document.getElementById(id);
-}
-async function loadData(){
- const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
- const response = await fetch(url);
- const issue = await response.json();
- displayData(issue.data);
+const getElementById = (id) => {
+  return document.getElementById(id);
 };
-const displayData = (data) =>{
-const issueContainer = getElementById("issues-container");
-issueContainer.innerHTML = "";
-data.forEach((element)=>{
+let activeTab = "all";
+let totalIssue;
+function handleTabChange(tab) {
+  activeTab = tab;
+  loadData(tab);
+}
+
+getElementById("all-btn").addEventListener("click", () => {
+  handleTabChange("all");
+});
+getElementById("open-btn").addEventListener("click", () => {
+  handleTabChange("open");
+});
+getElementById("close-btn").addEventListener("click", () => {
+  handleTabChange("closed");
+});
+
+async function loadData(activeStatus) {
+  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+  const response = await fetch(url);
+  const issue = await response.json();
+  if (activeStatus === "all") {
+    totalIssue = issue.data.length;
+    displayData(issue.data);
+    return;
+  }
+  const filteredData = issue.data.filter(
+    (issue) => issue.status === activeStatus,
+  );
+  totalIssue = filteredData.length;
+  console.log("filter", totalIssue);
+  displayData(filteredData);
+}
+const displayData = (data) => {
+  const issueContainer = getElementById("issues-container");
+  issueContainer.innerHTML = "";
+  getElementById("total-issue-count").innerHTML = totalIssue;
+  data.forEach((element) => {
     const cardWrapper = document.createElement("div");
 
-    cardWrapper.innerHTML=`
+    cardWrapper.innerHTML = `
     
     <!-- issue card  -->
           <div
-            class="card border-t-4 ${element.status === "open"? "border-success":"border-[#A855F7]"}  flex flex-col justify-start items-center rounded shadow"
+            class="card border-t-4 ${element.status === "open" ? "border-success" : "border-[#A855F7]"}  flex flex-col justify-start items-center rounded shadow"
           >
             <!-- card top -->
             <div class="p-4 space-y-3 w-full">
               <!-- 1 no div -->
               <div class="flex justify-between items-center w-full">
-                <img src=${element.status === "open"? `"./assets/Open-Status.png"`: `"./assets/ClosedStatus.png"`} alt="" />
+                <img src=${element.status === "open" ? `"./assets/Open-Status.png"` : `"./assets/ClosedStatus.png"`} alt="" />
                 <p
                   class="${
-                    element.priority=== "high" ? "text-error" : element.priority === "medium" ? "text-[#F59E0B]" : "text-[#9CA3AF]"
+                    element.priority === "high"
+                      ? "text-error"
+                      : element.priority === "medium"
+                        ? "text-[#F59E0B]"
+                        : "text-[#9CA3AF]"
                   } py-[6px] text-xs w-[80px] uppercase text-center rounded-full ${
-                    element.priority=== "high" ? "bg-[#FEECEC]" : element.priority === "medium" ? "bg-[#FFF6D1]" : "bg-[#EEEFF2]"
+                    element.priority === "high"
+                      ? "bg-[#FEECEC]"
+                      : element.priority === "medium"
+                        ? "bg-[#FFF6D1]"
+                        : "bg-[#EEEFF2]"
                   }"
                 >
                   ${element.priority}
@@ -46,14 +83,15 @@ data.forEach((element)=>{
                 </div>
                 <!-- card label wrapper -->
                 <div class="flex gap-1 w-full flex-wrap">
-                 ${element.labels.map((label)=>{
-                    
-                    return  `<p
-                    class="flex justify-start items-center max-w-fit gap-1 ${label=== "bug" ? "text-error": label==="help wanted"? "text-[#D97706]":"text-[#d96c06]"}  py-[6px] px-2 text-xs uppercase text-center rounded-full ${label=== "bug" ? "bg-[#FEECEC]": label==="help wanted"? "bg-[#FFF8DB]":"bg-green-200"} border ${label=== "bug" ? "border-error": label==="help wanted"? "border-[#D97706]":"border-green-500"} "
+                 ${element.labels
+                   .map((label) => {
+                     return `<p
+                    class="flex justify-start items-center max-w-fit gap-1 ${label === "bug" ? "text-error" : label === "help wanted" ? "text-[#D97706]" : "text-[#d96c06]"}  py-[6px] px-2 text-xs uppercase text-center rounded-full ${label === "bug" ? "bg-[#FEECEC]" : label === "help wanted" ? "bg-[#FFF8DB]" : "bg-green-200"} border ${label === "bug" ? "border-error" : label === "help wanted" ? "border-[#D97706]" : "border-green-500"} "
                   >
                     <img src="./assets/BugDroid.png" alt="" /> ${label}
-                  </p>`
-                 }).join("")}
+                  </p>`;
+                   })
+                   .join("")}
                   
                 </div>
               </div>
@@ -70,10 +108,9 @@ data.forEach((element)=>{
           <!-- issue card end here  -->
     `;
     issueContainer.appendChild(cardWrapper);
-})
+  });
 };
-loadData()
-console.log("just checking that if it published auto or not")
+// loadData("https://phi-lab-server.vercel.app/api/v1/lab/issues")
 
 // "id": 1,
 // "title": "Fix navigation menu on mobile devices",
